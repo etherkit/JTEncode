545a012f23e628bfcf6ce5d2fd818719c14bba43
Jason Milldrum <milldrum@gmail.com>
2015-10-02 17:52:17 -0700

    Fix si5351.init in example sketch

--- examples/Si5351JTDemo/Si5351JTDemo.ino
+++ examples/Si5351JTDemo/Si5351JTDemo.ino
@@ -9,11 +9,11 @@
 // Connect a momentary push button to pin 12 to use as the
 // transmit trigger. Get fancy by adding your own code to trigger
 // off of the time from a GPS or your PC via virtual serial.
-// 
-// Original code based on Feld Hell beacon for Arduino by Mark 
-// Vandewettering K6HX, adapted for the Si5351A by Robert 
+//
+// Original code based on Feld Hell beacon for Arduino by Mark
+// Vandewettering K6HX, adapted for the Si5351A by Robert
 // Liesenfeld AK6L <ak6l@ak6l.org>.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -21,10 +21,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject
 // to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -51,7 +51,7 @@
 #define JT9_CTC                 9000          // CTC value for JT9-1
 #define JT65_CTC                5812          // CTC value for JT65A
 #define JT4_CTC                 3578          // CTC value for JT4
-#define WSPR_CTC                10672         // CTC value for WSPR  
+#define WSPR_CTC                10672         // CTC value for WSPR
 
 #define JT9_DEFAULT_FREQ        14078600UL
 #define JT65_DEFAULT_FREQ       14077500UL
@@ -92,15 +92,15 @@ ISR(TIMER1_COMPA_vect)
 {
     proceed = true;
 }
- 
+
 // Loop through the string, transmitting one character at a time.
 void encode()
 {
   uint8_t i;
-  
+
   // Clear out the old transmit buffer
   memset(tx_buffer, 0, 255);
-  
+
   // Set the proper frequency and timer CTC depending on mode
   switch(cur_mode)
   {
@@ -117,11 +117,11 @@ void encode()
     jtencode.wspr_encode(call, loc, dbm, tx_buffer);
     break;
   }
-  
+
   // Reset the tone to the base frequency and turn on the output
   si5351.output_enable(SI5351_CLK0, 1);
   digitalWrite(LED_PIN, HIGH);
-  
+
   // Now transmit the channel symbols
   for(i = 0; i < symbol_count; i++)
   {
@@ -129,13 +129,13 @@ void encode()
       proceed = false;
       while(!proceed);
   }
-      
+
   // Turn off the output
   si5351.output_enable(SI5351_CLK0, 0);
   digitalWrite(LED_PIN, LOW);
 }
 
- 
+
 void setup()
 {
   // Use the Arduino's on-board LED as a keying indicator.
@@ -144,10 +144,10 @@ void setup()
 
   // Use a button connected to pin 12 as a transmit trigger
   pinMode(BUTTON, INPUT_PULLUP);
-  
+
   //Serial.begin(57600);
 
-  // Set the proper frequency, tone spacing, symbol count, and 
+  // Set the proper frequency, tone spacing, symbol count, and
   // timer CTC depending on mode
   switch(cur_mode)
   {
@@ -176,18 +176,18 @@ void setup()
     tone_spacing = WSPR_TONE_SPACING;
     break;
   }
-      
+
   // Initialize the Si5351
   // Change the 2nd parameter in init if using a ref osc other
   // than 25 MHz
-  si5351.init(SI5351_VARIANT_A3, SI5351_CRYSTAL_LOAD_8PF, 0);
+  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0);
 
   // Set CLK0 output
   si5351.set_correction(0);
   si5351.set_freq(freq * 100, 0, SI5351_CLK0);
   si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_8MA); // Set for max power if desired
   si5351.output_enable(SI5351_CLK0, 0); // Disable the clock initially
-  
+
   // Set up Timer1 for interrupts every symbol period.
   noInterrupts();          // Turn off interrupts.
   TCCR1A = 0;              // Set entire TCCR1A register to 0; disconnects
@@ -202,7 +202,7 @@ void setup()
   OCR1A = ctc;             // Set up interrupt trigger count;
   interrupts();            // Re-enable interrupts.
 }
- 
+
 void loop()
 {
   // Debounce the button and trigger TX on push
@@ -212,7 +212,7 @@ void loop()
     if (digitalRead(BUTTON) == LOW)
     {
       encode();
-      
+
       delay(50); //delay to avoid extra triggers
     }
   }
